f57be5d3679429fe70077370270d721102f3519a
Mason Eischens <eisch115@morris.umn.edu>
2022-10-04 16:27:34 -0500

    Implemented array-merge almost completely

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,6 +1,33 @@
 #include "array_merge.h"
 
+int contains(int* array, int value, int size){
+	for (int i = 0; i < size; i++){
+		if (array[i] == value){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int sum(int num_arrays, int* sizes) {
+	int total = 0;
+	for (int i = 0; i < num_arrays; i++) {
+		total += sizes[i];
+	}
+	return total;
+}
+
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
-  return sizes;
+	int unique[] = sum(num_arrays, sizes);
+	unique = (int*) calloc(num_arrays, sizeof(int));
+	int counter = 0;
+	for (int i = 0; i < num_arrays; i++){
+		for(int j = 0; j < sizes[i]; j++) {
+			if(contains(values[i], values[i][j], sizes[i]) == 0) {
+				unique[counter] = values[i][j];
+				counter++;	
+			}
+		}
+	}	
+  	return sizes;
 }
