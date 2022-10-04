6425f97ff13ec208b33cb12bd60c9d877ae6c505
Mason Eischens <eisch115@morris.umn.edu>
2022-10-04 15:22:28 -0500

    wrote mergesort method according to java example

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,12 +1,48 @@
 #include "mergesort.h"
 
 void mergesort(int size, int values[]) {
-  // This obviously doesn't actually do any *sorting*, so there's
-  // certainly work still to be done.
-  //
-  // Remember that a key goal here is to learn to use
-  // `malloc/calloc` and `free`, so make sure you explicitly
-  // allocate any new arrays that you need, even if you
-  // might not strictly need to.
-  return;
+  mergesortRange(values, 0, size);
+}
+
+void mergesortRange(int values[], int startIndex, int endIndex){
+	int rangeSize = endIndex - startIndex;
+	if (rangeSize >= 2){
+		int midPoint = (startIndex + endIndex) / 2;
+		mergesortRange(values, startIndex, midPoint);
+		mergesortRange(values, midPoint, endIndex);
+		mergeRanges(values, startIndex, midPoint, endIndex);
+	}
+}
+
+void mergeRanges(int[] values, int startIndex, int midPoint, int endIndex){
+	const int rangeSize = endIndex - startIndex; 
+	destination = (int*) calloc(rangeSize, sizeOf(int));
+	int firstIndex = startIndex;
+	int secondIndex = midPoint;
+	int copyIndex = 0;
+	while (firstIndex < midPoint && secondIndex < endIndex){
+		if (values[firstIndex] < values[secondIndex]){
+			destination[copyIndex] = values[firstIndex];
+			firstIndex++;
+		}
+		else {
+			destination[copyIndex] = values[secondIndex];
+			secondIndex++;	
+		}
+		copyIndex++;
+	}
+	while (firstIndex < midPoint){
+		destination[copyIndex] = values[firstIndex];
+		copyIndex++;
+		firstIndex++;
+	}
+	while (secondIndex < endIndex){
+		destination[copyIndex] = values[secondIndex];
+		copyIndex++;
+		secondIndex++;
+	}
+	for (int i = 0; i < rangeSize; ++i){
+		values[i + startIndex] = destination[i];
+	}
+	free(destination);
 }
